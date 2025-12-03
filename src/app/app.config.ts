34930111db0bdb3bb1300528d6c5e288e324dce1
VOLO-DC\anahit.khachatryan <anahit.khachatryan@volo.global>
2025-12-03 12:53:59 +0400

    add primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
@@ -15,6 +15,57 @@ import { signalrFeature } from './core/store/features/signalr.feature';
 import * as AuthEffects from './core/store/effects/auth.effects';
 import * as SignalREffects from './core/store/effects/signalr.effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+
+
+const Noir = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{slate.50}',
+      100: '{slate.100}',
+      200: '{slate.200}',
+      300: '{slate.300}',
+      400: '{slate.400}',
+      500: '{slate.500}',
+      600: '{slate.600}',
+      700: '{slate.700}',
+      800: '{slate.800}',
+      900: '{slate.900}',
+      950: '{slate.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{slate.950}',
+          inverseColor: '#ffffff',
+          hoverColor: '{slate.900}',
+          activeColor: '{slate.800}',
+        },
+        highlight: {
+          background: '{slate.950}',
+          focusBackground: '{slate.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+      },
+      dark: {
+        primary: {
+          color: '{slate.50}',
+          inverseColor: '{slate.950}',
+          hoverColor: '{slate.100}',
+          activeColor: '{slate.200}',
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)',
+        },
+      },
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -35,6 +86,20 @@ export const appConfig: ApplicationConfig = {
         },
       },
     ),
+    providePrimeNG({
+      theme: {
+        preset: Noir,
+        options: {
+          darkModeSelector: 'html.dark',
+        },
+      },
+      zIndex: {
+        modal: 1100, // dialog, sidebar
+        overlay: 1200, // dropdown, overlaypanel
+        menu: 1000, // overlay menus
+        tooltip: 1100, // tooltip
+      },
+    }),
     provideState(authFeature),
     provideEffects([AuthEffects]),
     provideState(authFeature),
